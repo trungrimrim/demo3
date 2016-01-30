3d0b5337f797b56743bd8c04cc2927f59e3d8dc3
rimrim <dinhquangrim@gmail.com>
2016-01-30 17:18:54 +1100

    add app for lists

---
+++ lists/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestSmoke(TestCase):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_name(self):
+        self.assertEqual(2, 1)
