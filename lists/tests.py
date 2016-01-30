102c3ff3c92393a50f05ce296c74e52cf949def4
rimrim <dinhquangrim@gmail.com>
2016-01-30 19:21:03 +1100

    first item model

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 
+from lists.models import Item
+
 # Create your tests here.
 class TestSmoke(TestCase):
 
@@ -54,3 +56,28 @@ class TestHomePage(TestCase):
         expected_html = render_to_string('home.html',
                 {'new_item_text': '4 Minjah ct'})
         self.assertEqual(expected_html,response.content.decode())
+
+class TestItemModel(TestCase):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved = saved_items[0]
+        self.assertEqual(first_saved.text,'The first item')
