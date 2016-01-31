e61b998b276178bee882b8acd5a18d8d944e99b4
rimrim <dinhquangrim@gmail.com>
2016-01-31 20:24:24 +1100

    use base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,7 +27,14 @@ class TestNewAgent(LiveServerTestCase):
         self.assertIn(row_text,[row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        #Heng goes to the home page
         self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #He notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + \
+                inputbox.size['width'] /2 ,512,delta=5)
 
         # he notices the page title and header mention 'real-estate' lists
         assert 'Property' in self.browser.title, "Title was " + self.browser.title
