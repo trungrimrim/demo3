20abeb5c6583ea6474937705f79e61a532b5b104
trung dinh <trung.dinh@scramteam.com>
2016-01-31 00:12:54 -0800

    full site

--- lists/urls.py
+++ lists/urls.py
@@ -16,15 +16,14 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from lists import views as list_views
-from lists import urls as list_urls
+from lists import views
 
 urlpatterns = [
-    url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/', include(list_urls)),
-    #url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    #url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
-    #url(r'^lists/new$', views.new_list, name='new_list')
+    #url(r'^$', views.home_page, name='home'),
+    #url(r'^lists/$', include(list_urls)),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^new$', views.new_list, name='new_list')
 
     #url(r'^admin/', include(admin.site.urls)),
 ]
